33e853db2cbbb0c3c9f945372bbc40eb869f470b
McNeelands <m.mcneelands@hotmail.com>
2016-03-15 13:54:23 -0400

    Mitchell's First upload(login/signup page)

--- dev_prep/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :prep_courses
-  root 'prep_courses#login'
-  get '/html' => 'prep_courses#html'
-  get '/javascript' => 'prep_courses#javascript'
-  get '/git' => 'prep_courses#git'
-  get '/ruby' => 'prep_courses#ruby'
-
-end
